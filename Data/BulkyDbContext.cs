b88800bf93fa73880f41bf1931350c395ca6d468
Marcos Freire <Marcos@dmsgroup.ca>
2022-03-19 21:04:14 -0700

    Add project files.

---
+++ Data/BulkyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class BulkyDbContext : DbContext
+    {
+        public BulkyDbContext(DbContextOptions<BulkyDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(60)]
+        public string Name { get; set; } = null!;
+        [Range(0,30)]
+        [Display(Name ="Display Order")]
+        public int DisplayOrder { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
